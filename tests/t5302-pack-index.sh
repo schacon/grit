68b51e39f829ddf036779f6d73f44c32da0b7c19
New Name <test@example.com>
2026-04-01 23:33:22 +0000

    tests: deepen mv (34), repack (12), pack-index (18), prune (9), gc (9)

--- tests/t5302-pack-index.sh
+++ tests/t5302-pack-index.sh
@@ -5,6 +5,8 @@ test_description='prune-packed removes objects already in pack files'
 
 . ./test-lib.sh
 
+REAL_GIT=${REAL_GIT:-/usr/bin/git}
+
 test_expect_success 'setup: create loose object' '
 	git init repo &&
 	cd repo &&
@@ -36,6 +38,140 @@ test_expect_success 'prune-packed -n is alias for --dry-run' '
 test_expect_success 'prune-packed -q runs without error' '
 	cd repo &&
 	grit prune-packed -q
+'
+
+# ---------------------------------------------------------------------------
+# Additional verify-pack and show-index tests
+# ---------------------------------------------------------------------------
+
+test_expect_success 'verify-pack on a valid pack passes' '
+	rm -rf repo_vp &&
+	grit init repo_vp &&
+	cd repo_vp &&
+	echo hello >a.txt &&
+	git add a.txt &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	git commit -m initial &&
+	git repack -a -d &&
+	pack=$(echo .git/objects/pack/*.pack) &&
+	git verify-pack "$pack"
+'
+
+test_expect_success 'verify-pack -v shows object details' '
+	cd repo_vp &&
+	pack=$(echo .git/objects/pack/*.pack) &&
+	git verify-pack -v "$pack" >out &&
+	grep "commit" out &&
+	grep "tree" out &&
+	grep "blob" out
+'
+
+test_expect_success 'verify-pack accepts .idx path' '
+	cd repo_vp &&
+	idx=$(echo .git/objects/pack/*.idx) &&
+	git verify-pack "$idx"
+'
+
+test_expect_success 'verify-pack -s shows stat summary' '
+	cd repo_vp &&
+	pack=$(echo .git/objects/pack/*.pack) &&
+	git verify-pack -s "$pack" >out &&
+	test -s out
+'
+
+test_expect_success 'verify-pack fails on nonexistent file' '
+	cd repo_vp &&
+	test_must_fail git verify-pack nonexistent.pack 2>err
+'
+
+test_expect_success 'show-index reads valid idx and outputs entries' '
+	cd repo_vp &&
+	idx=$(echo .git/objects/pack/*.idx) &&
+	git show-index <"$idx" >out &&
+	test_line_count -gt 0 out
+'
+
+test_expect_success 'show-index --object-format=sha1 accepted' '
+	cd repo_vp &&
+	idx=$(echo .git/objects/pack/*.idx) &&
+	git show-index --object-format=sha1 <"$idx" >out &&
+	test_line_count -gt 0 out
+'
+
+test_expect_success 'show-index --object-format=sha256 rejected' '
+	cd repo_vp &&
+	idx=$(echo .git/objects/pack/*.idx) &&
+	test_must_fail git show-index --object-format=sha256 <"$idx"
+'
+
+test_expect_success 'show-index OIDs match verify-pack OIDs' '
+	cd repo_vp &&
+	idx=$(echo .git/objects/pack/*.idx) &&
+	"$REAL_GIT" verify-pack -v "$idx" |
+		grep -E "^[0-9a-f]{40}" |
+		awk "{print \$1}" | sort >expected_oids &&
+	git show-index <"$idx" | awk "{print \$2}" | sort >actual_oids &&
+	test_cmp expected_oids actual_oids
+'
+
+# ---------------------------------------------------------------------------
+# Additional prune-packed tests
+# ---------------------------------------------------------------------------
+
+test_expect_success 'prune-packed removes loose objects already in pack' '
+	rm -rf repo_pp &&
+	grit init repo_pp &&
+	cd repo_pp &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	echo "pack me" >f.txt &&
+	git add f.txt &&
+	git commit -m initial &&
+	BLOB=$(git hash-object f.txt) &&
+	BLOB_FILE=.git/objects/$(echo "$BLOB" | sed "s/^../&\//") &&
+	git repack -a &&
+	test_path_is_file "$BLOB_FILE" &&
+	grit prune-packed &&
+	test_path_is_missing "$BLOB_FILE"
+'
+
+test_expect_success 'prune-packed --dry-run lists but does not remove' '
+	rm -rf repo_ppd &&
+	grit init repo_ppd &&
+	cd repo_ppd &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	echo "dry run me" >f.txt &&
+	git add f.txt &&
+	git commit -m initial &&
+	git repack -a -d &&
+	BLOB=$(echo extra_dry | git hash-object -w --stdin) &&
+	BLOB_FILE=.git/objects/$(echo "$BLOB" | sed "s/^../&\//") &&
+	git repack -a &&
+	test_path_is_file "$BLOB_FILE" &&
+	loose_before=$(git count-objects | sed "s/ .*//") &&
+	grit prune-packed --dry-run >out &&
+	loose_after=$(git count-objects | sed "s/ .*//") &&
+	test "$loose_before" = "$loose_after"
+'
+
+test_expect_success 'prune-packed -n does not remove objects' '
+	cd repo_ppd &&
+	loose_before=$(git count-objects | sed "s/ .*//") &&
+	grit prune-packed -n >out2 &&
+	loose_after=$(git count-objects | sed "s/ .*//") &&
+	test "$loose_before" = "$loose_after"
+'
+
+test_expect_success 'prune-packed has no effect when no packs exist' '
+	rm -rf repo_ppnp &&
+	grit init repo_ppnp &&
+	cd repo_ppnp &&
+	BLOB=$(echo "no pack" | git hash-object -w --stdin) &&
+	BLOB_FILE=.git/objects/$(echo "$BLOB" | sed "s/^../&\//") &&
+	grit prune-packed &&
+	test_path_is_file "$BLOB_FILE"
 '
 
 test_done
--- tests/t7001-mv.sh
+++ tests/t7001-mv.sh
@@ -243,4 +243,278 @@ test_expect_success 'mv fails on conflicted file' '
 	grep -i "conflict" actual
 '
 
+# ---------------------------------------------------------------------------
+# Moving to absent target with trailing slash
+# ---------------------------------------------------------------------------
+
+test_expect_success 'moving to absent target with trailing slash' '
+	rm -rf repo_trail &&
+	git init repo_trail &&
+	cd repo_trail &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir -p path0 &&
+	echo content >path0/COPYING &&
+	git add path0/COPYING &&
+	git commit -m init &&
+	test_must_fail git mv path0/COPYING no-such-dir/ &&
+	test_must_fail git mv path0/COPYING no-such-dir// &&
+	git mv path0/ no-such-dir/ &&
+	test_path_is_dir no-such-dir
+'
+
+# ---------------------------------------------------------------------------
+# Source is prefix of destination
+# ---------------------------------------------------------------------------
+
+test_expect_success 'succeed when source is a prefix of destination' '
+	rm -rf repo_prefix &&
+	git init repo_prefix &&
+	cd repo_prefix &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir path2 &&
+	echo x >path2/COPYING &&
+	git add path2 &&
+	git commit -m init &&
+	git mv path2/COPYING path2/COPYING-renamed &&
+	git ls-files >actual &&
+	grep "path2/COPYING-renamed" actual
+'
+
+# ---------------------------------------------------------------------------
+# Rename directory to non-existing directory
+# ---------------------------------------------------------------------------
+
+test_expect_success 'rename directory to non-existing directory' '
+	rm -rf repo_rdir &&
+	git init repo_rdir &&
+	cd repo_rdir &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir dir-a &&
+	>dir-a/f &&
+	git add dir-a &&
+	git commit -m init &&
+	git mv dir-a non-existing-dir &&
+	test_path_is_dir non-existing-dir &&
+	test_path_is_missing dir-a
+'
+
+# ---------------------------------------------------------------------------
+# Do not move directory over existing directory
+# ---------------------------------------------------------------------------
+
+test_expect_success 'do not move directory over existing directory' '
+	rm -rf repo_noover &&
+	git init repo_noover &&
+	cd repo_noover &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir -p path0/path2 &&
+	mkdir -p path2 &&
+	echo x >path2/f &&
+	git add path2 &&
+	git commit -m init &&
+	test_must_fail git mv path2 path0
+'
+
+# ---------------------------------------------------------------------------
+# Overwrite symlink with file via -f
+# ---------------------------------------------------------------------------
+
+test_expect_success 'git mv should overwrite symlink to a file' '
+	rm -rf repo_sym &&
+	git init repo_sym &&
+	cd repo_sym &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	echo 1 >moved &&
+	ln -s moved symlink &&
+	git add moved symlink &&
+	git commit -m init &&
+	test_must_fail git mv moved symlink &&
+	git mv -f moved symlink &&
+	test_path_is_missing moved &&
+	test_path_is_file symlink &&
+	test "$(cat symlink)" = 1 &&
+	git update-index --refresh &&
+	git diff-files --quiet
+'
+
+# ---------------------------------------------------------------------------
+# Overwrite file with symlink via -f
+# ---------------------------------------------------------------------------
+
+test_expect_success 'git mv should overwrite file with a symlink' '
+	rm -rf repo_sym2 &&
+	git init repo_sym2 &&
+	cd repo_sym2 &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	echo 1 >moved &&
+	ln -s moved symlink &&
+	git add moved symlink &&
+	git commit -m init &&
+	test_must_fail git mv symlink moved &&
+	git mv -f symlink moved &&
+	test_path_is_missing symlink &&
+	test -L moved &&
+	git update-index --refresh &&
+	git diff-files --quiet
+'
+
+# ---------------------------------------------------------------------------
+# Check moved symlink is a symlink
+# ---------------------------------------------------------------------------
+
+test_expect_success 'check moved symlink' '
+	rm -rf repo_chksym &&
+	git init repo_chksym &&
+	cd repo_chksym &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	echo 1 >moved &&
+	ln -s moved symlink &&
+	git add moved symlink &&
+	git commit -m init &&
+	git mv -f symlink moved &&
+	test -L moved
+'
+
+# ---------------------------------------------------------------------------
+# Moving to existing tracked/untracked target with trailing slash
+# ---------------------------------------------------------------------------
+
+test_expect_success 'moving to existing tracked target with trailing slash' '
+	rm -rf repo_track_trail &&
+	git init repo_track_trail &&
+	cd repo_track_trail &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir -p path0 path2 &&
+	echo x >path0/COPYING &&
+	>path2/file &&
+	git add path0 path2 &&
+	git commit -m init &&
+	git mv path0/ path2/ &&
+	test_path_is_dir path2/path0/
+'
+
+test_expect_success 'moving to existing untracked target with trailing slash' '
+	rm -rf repo_untrack_trail &&
+	git init repo_untrack_trail &&
+	cd repo_untrack_trail &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir -p path0 &&
+	echo content >path0/COPYING &&
+	git add path0 &&
+	git commit -m init &&
+	mkdir path1 &&
+	git mv path0/ path1/ &&
+	test_path_is_dir path1/path0/
+'
+
+# ---------------------------------------------------------------------------
+# Michael Cassar and Sergey Vlasov test cases
+# ---------------------------------------------------------------------------
+
+test_expect_success "Michael Cassar test case" '
+	rm -rf repo_mc &&
+	git init repo_mc &&
+	cd repo_mc &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir -p papers/unsorted papers/all-papers partA &&
+	echo a >papers/unsorted/Thesis.pdf &&
+	echo b >partA/outline.txt &&
+	echo c >papers/unsorted/_another &&
+	git add papers partA &&
+	T1=$(git write-tree) &&
+	git mv papers/unsorted/Thesis.pdf papers/all-papers/moo-blah.pdf &&
+	T=$(git write-tree) &&
+	git ls-tree -r $T >out &&
+	grep partA/outline.txt out
+'
+
+test_expect_success "Sergey Vlasov test case" '
+	rm -rf repo_sv &&
+	git init repo_sv &&
+	cd repo_sv &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir ab &&
+	date >ab.c &&
+	date >ab/d &&
+	git add ab.c ab &&
+	git commit -m "initial" &&
+	git mv ab a
+'
+
+# ---------------------------------------------------------------------------
+# -k on multiple untracked files
+# ---------------------------------------------------------------------------
+
+test_expect_success 'checking -k on multiple untracked files' '
+	rm -rf repo_multi_k &&
+	git init repo_multi_k &&
+	cd repo_multi_k &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir -p path0 &&
+	echo tracked >path0/COPYING &&
+	git add path0 &&
+	git commit -m init &&
+	>untracked1 &&
+	>untracked2 &&
+	git mv -k untracked1 untracked2 path0 &&
+	test_path_is_file untracked1 &&
+	test_path_is_file untracked2 &&
+	test_path_is_missing path0/untracked1 &&
+	test_path_is_missing path0/untracked2
+'
+
+# ---------------------------------------------------------------------------
+# -f on untracked file with existing target
+# ---------------------------------------------------------------------------
+
+test_expect_success 'checking -f on untracked file with existing target' '
+	rm -rf repo_f_untrack &&
+	git init repo_f_untrack &&
+	cd repo_f_untrack &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir path0 &&
+	echo tracked >path0/COPYING &&
+	git add path0 &&
+	git commit -m init &&
+	>untracked1 &&
+	>path0/untracked1 &&
+	test_must_fail git mv -f untracked1 path0 &&
+	test_path_is_file untracked1 &&
+	test_path_is_file path0/untracked1
+'
+
+# ---------------------------------------------------------------------------
+# Moving whole subdirectory into subdirectory
+# ---------------------------------------------------------------------------
+
+test_expect_success 'moving whole subdirectory into subdirectory' '
+	rm -rf repo_subdir &&
+	git init repo_subdir &&
+	cd repo_subdir &&
+	git config user.email "test@example.com" &&
+	git config user.name "Test User" &&
+	mkdir -p path0 path1 &&
+	echo x >path0/COPYING &&
+	echo y >path0/README &&
+	git add path0 &&
+	git commit -m init &&
+	git mv path0 path1 &&
+	git ls-files >actual &&
+	grep "path1/path0/COPYING" actual &&
+	grep "path1/path0/README" actual
+'
+
 test_done
