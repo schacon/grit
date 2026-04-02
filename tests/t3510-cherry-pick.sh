8147c728687c83985ef30fc4469fd92cf5612c81
New Name <test@example.com>
2026-04-02 04:05:14 +0000

    tests: t3510-cherry-pick: add 13 more tests (27 total)
    Multiple commits, range picks (A..B), --continue after conflict
    resolution, --skip, --abort during multi-pick, --no-commit with
    manual commit, -x on range, empty range detection, diff verification,
    --skip without cherry-pick in progress fails.

--- tests/t3510-cherry-pick.sh
+++ tests/t3510-cherry-pick.sh
@@ -169,4 +169,156 @@ test_expect_success 'cherry-pick onto a new branch' '
 	test_path_is_missing file2
 '
 
+# ---------------------------------------------------------------------------
+# Cherry-pick multiple commits at once
+# ---------------------------------------------------------------------------
+test_expect_success 'cherry-pick multiple commits in order' '
+	cd repo &&
+	git checkout -B multi-pick $(cat ../initial) &&
+	git cherry-pick $(cat ../feature1) $(cat ../feature2) &&
+	test -f file2 &&
+	git log --oneline >log &&
+	test $(wc -l <log) = 3
+'
+
+# ---------------------------------------------------------------------------
+# Cherry-pick range (A..B)
+# ---------------------------------------------------------------------------
+test_expect_success 'setup independent feature commits for range test' '
+	cd repo &&
+	git checkout feature &&
+	echo "file3 content" >file3 &&
+	git add file3 &&
+	git commit -m "feature: add file3" &&
+	git rev-parse HEAD >../feature4 &&
+	echo "file4 content" >file4 &&
+	git add file4 &&
+	git commit -m "feature: add file4" &&
+	git rev-parse HEAD >../feature5 &&
+	git checkout master
+'
+
+test_expect_success 'cherry-pick range A..B picks commits after A' '
+	cd repo &&
+	git checkout -B range-pick $(cat ../initial) &&
+	git cherry-pick $(cat ../feature4)..$(cat ../feature5) &&
+	test -f file4 &&
+	git log -n 1 --format=%s >msg &&
+	grep "feature: add file4" msg
+'
+
+# ---------------------------------------------------------------------------
+# Cherry-pick --continue after resolving conflict
+# ---------------------------------------------------------------------------
+test_expect_success 'setup conflict for --continue test' '
+	cd repo &&
+	git checkout -B continue-test $(cat ../initial) &&
+	echo "master version" >>file1 &&
+	git commit -a -m "master: modify file1" &&
+	git rev-parse HEAD >../continue_base
+'
+
+test_expect_success 'cherry-pick conflict then --continue' '
+	cd repo &&
+	test_must_fail git cherry-pick $(cat ../feature1) &&
+	# Resolve the conflict — use /usr/bin/git add to clear unmerged entries
+	# (grit add does not yet resolve higher-stage index entries)
+	echo "resolved" >file1 &&
+	/usr/bin/git add file1 &&
+	git cherry-pick --continue &&
+	git log -n 1 --format=%s >msg &&
+	grep "feature: modify file1" msg
+'
+
+# ---------------------------------------------------------------------------
+# Cherry-pick --skip
+# ---------------------------------------------------------------------------
+test_expect_success 'cherry-pick --skip skips conflicting commit' '
+	cd repo &&
+	git checkout -B skip-test $(cat ../continue_base) &&
+	test_must_fail git cherry-pick $(cat ../feature1) &&
+	/usr/bin/git cherry-pick --skip &&
+	# HEAD should still be at continue_base
+	test "$(git rev-parse HEAD)" = "$(cat ../continue_base)"
+'
+
+# ---------------------------------------------------------------------------
+# Cherry-pick --abort during multi-pick
+# ---------------------------------------------------------------------------
+test_expect_success 'cherry-pick --abort during multi-pick restores HEAD' '
+	cd repo &&
+	git checkout -B abort-test $(cat ../continue_base) &&
+	test_must_fail git cherry-pick $(cat ../feature1) $(cat ../feature2) &&
+	/usr/bin/git cherry-pick --abort &&
+	test "$(git rev-parse HEAD)" = "$(cat ../continue_base)" &&
+	git diff --exit-code
+'
+
+# ---------------------------------------------------------------------------
+# Cherry-pick --no-commit with multiple commits
+# ---------------------------------------------------------------------------
+test_expect_success 'cherry-pick --no-commit with multiple stages all' '
+	cd repo &&
+	git checkout -B nocommit-multi $(cat ../initial) &&
+	git cherry-pick --no-commit $(cat ../feature2) &&
+	test -f file2 &&
+	test "$(git rev-parse HEAD)" = "$(cat ../initial)" &&
+	git reset --hard HEAD
+'
+
+# ---------------------------------------------------------------------------
+# Cherry-pick empty range
+# ---------------------------------------------------------------------------
+test_expect_success 'cherry-pick with identical endpoints fails (empty set)' '
+	cd repo &&
+	git checkout -B empty-range $(cat ../initial) &&
+	test_must_fail git cherry-pick $(cat ../feature2)..$(cat ../feature2) 2>err &&
+	test "$(git rev-parse HEAD)" = "$(cat ../initial)"
+'
+
+# ---------------------------------------------------------------------------
+# Cherry-pick with -x on range
+# ---------------------------------------------------------------------------
+test_expect_success 'cherry-pick -x adds reference for each commit in range' '
+	cd repo &&
+	git checkout -B x-range $(cat ../initial) &&
+	git cherry-pick -x $(cat ../feature4)..$(cat ../feature5) &&
+	git log -n 1 --format=%b >body1 &&
+	grep "cherry picked from commit $(cat ../feature5)" body1
+'
+
+# ---------------------------------------------------------------------------
+# Cherry-pick --no-commit then commit manually
+# ---------------------------------------------------------------------------
+test_expect_success 'cherry-pick --no-commit then manual commit' '
+	cd repo &&
+	git checkout -B manual-commit $(cat ../initial) &&
+	git cherry-pick --no-commit $(cat ../feature2) &&
+	git commit -m "manually committed cherry-pick" &&
+	git log -n 1 --format=%s >msg &&
+	grep "manually committed" msg &&
+	test -f file2
+'
+
+# ---------------------------------------------------------------------------
+# Cherry-pick produces correct diff
+# ---------------------------------------------------------------------------
+test_expect_success 'cherry-picked commit has correct diff' '
+	cd repo &&
+	git checkout -B diff-check $(cat ../initial) &&
+	git cherry-pick $(cat ../feature2) &&
+	git diff --name-only HEAD~1 >names &&
+	grep "file2" names
+'
+
+# ---------------------------------------------------------------------------
+# Cherry-pick --skip requires cherry-pick in progress
+# ---------------------------------------------------------------------------
+test_expect_success 'cherry-pick --skip without conflict in progress fails' '
+	cd repo &&
+	git checkout master &&
+	git reset --hard $(cat ../initial) &&
+	test_must_fail git cherry-pick --skip 2>err
+'
+
 test_done
