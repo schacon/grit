8dfd05dd7dbbc7cc1d3b5c7862e3802369077fcd
Local User <t@t.com>
2026-04-02 10:57:56 +0000

    tests: add t12610-rev-list-all-branches.sh (32 tests)
    Tests for grit rev-list --all with multiple branches: counting,
    deduplication, --reverse, --max-count, --skip, --first-parent,
    branch ranges, and topology changes.

---
+++ tests/t12610-rev-list-all-branches.sh
@@ -0,0 +1,211 @@
+#!/bin/sh
+
+test_description='grit rev-list --all with multiple branches and various topologies'
+
+cd "$(dirname "$0")" || exit 1
+. ./test-lib.sh
+
+test_expect_success 'setup linear history' '
+    grit init repo && cd repo &&
+    git config user.email "t@t.com" && git config user.name "T" &&
+    echo one >file.txt && grit add file.txt && grit commit -m "c1" &&
+    echo two >file2.txt && grit add file2.txt && grit commit -m "c2" &&
+    echo three >file3.txt && grit add file3.txt && grit commit -m "c3"
+'
+
+test_expect_success 'rev-list --all on single branch' '
+    (cd repo && grit rev-list --all >../actual) &&
+    (cd repo && git rev-list --all >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all --count on single branch' '
+    (cd repo && grit rev-list --count --all >../actual) &&
+    echo 3 >expect &&
+    test_cmp expect actual
+'
+
+test_expect_success 'setup second branch with divergent history' '
+    (cd repo && git branch branchA &&
+    git checkout branchA &&
+    echo a1 >a1.txt && grit add a1.txt && grit commit -m "a1" &&
+    echo a2 >a2.txt && grit add a2.txt && grit commit -m "a2" &&
+    git checkout master)
+'
+
+test_expect_success 'rev-list --all includes both branches' '
+    (cd repo && grit rev-list --all >../actual) &&
+    (cd repo && git rev-list --all >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all --count with two branches' '
+    (cd repo && grit rev-list --count --all >../actual) &&
+    echo 5 >expect &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all contains branchA commits' '
+    (cd repo && grit rev-list --all >../all_commits &&
+    branchA_tip=$(git rev-parse branchA) &&
+    grep "$branchA_tip" ../all_commits)
+'
+
+test_expect_success 'rev-list --all contains master commits' '
+    (cd repo && grit rev-list --all >../all_commits &&
+    master_tip=$(git rev-parse master) &&
+    grep "$master_tip" ../all_commits)
+'
+
+test_expect_success 'setup third branch' '
+    (cd repo && git branch branchB &&
+    git checkout branchB &&
+    echo b1 >b1.txt && grit add b1.txt && grit commit -m "b1" &&
+    git checkout master)
+'
+
+test_expect_success 'rev-list --all with three branches' '
+    (cd repo && grit rev-list --all >../actual) &&
+    (cd repo && git rev-list --all >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all --count with three branches' '
+    (cd repo && grit rev-list --count --all >../actual) &&
+    echo 6 >expect &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all --reverse' '
+    (cd repo && grit rev-list --all --reverse >../actual) &&
+    (cd repo && git rev-list --all --reverse >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all --reverse first is root' '
+    (cd repo && grit rev-list --all --reverse >../actual) &&
+    head -1 actual >first &&
+    (cd repo && git rev-list --all --reverse >../expect_all) &&
+    head -1 expect_all >expect_root &&
+    test_cmp expect_root first
+'
+
+test_expect_success 'rev-list --all --max-count=1' '
+    (cd repo && grit rev-list --all --max-count=1 >../actual) &&
+    wc -l <actual >count &&
+    echo 1 >expect_count &&
+    test_cmp expect_count count
+'
+
+test_expect_success 'rev-list --all --max-count=3' '
+    (cd repo && grit rev-list --all --max-count=3 >../actual) &&
+    (cd repo && git rev-list --all --max-count=3 >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all --skip=2' '
+    (cd repo && grit rev-list --all --skip=2 >../actual) &&
+    (cd repo && git rev-list --all --skip=2 >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all --skip=2 --max-count=2' '
+    (cd repo && grit rev-list --all --skip=2 --max-count=2 >../actual) &&
+    (cd repo && git rev-list --all --skip=2 --max-count=2 >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list master vs rev-list --all differs' '
+    (cd repo && grit rev-list master >../master_only) &&
+    (cd repo && grit rev-list --all >../all_commits) &&
+    ! test_cmp master_only all_commits
+'
+
+test_expect_success 'rev-list branchA vs --all differs' '
+    (cd repo && grit rev-list branchA >../branchA_only) &&
+    (cd repo && grit rev-list --all >../all_commits) &&
+    ! test_cmp branchA_only all_commits
+'
+
+test_expect_success 'rev-list --all deduplicates shared commits' '
+    (cd repo && grit rev-list --all >../actual) &&
+    sort actual >sorted &&
+    sort -u actual >sorted_uniq &&
+    test_cmp sorted_uniq sorted
+'
+
+test_expect_success 'rev-list master branchA union matches rev-list with both' '
+    (cd repo && grit rev-list master branchA >../actual) &&
+    (cd repo && git rev-list master branchA >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all --first-parent' '
+    (cd repo && grit rev-list --all --first-parent >../actual) &&
+    (cd repo && git rev-list --all --first-parent >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list branch range with --all context' '
+    (cd repo && grit rev-list master..branchA >../actual) &&
+    (cd repo && git rev-list master..branchA >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list branch range count master..branchA' '
+    (cd repo && grit rev-list --count master..branchA >../actual) &&
+    echo 2 >expect &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list branch range count master..branchB' '
+    (cd repo && grit rev-list --count master..branchB >../actual) &&
+    echo 1 >expect &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all output is valid hex hashes' '
+    (cd repo && grit rev-list --all >../actual) &&
+    while read hash; do
+        echo "$hash" | grep -q "^[0-9a-f]\{40\}$" || exit 1
+    done <actual
+'
+
+test_expect_success 'rev-list --all matches rev-list of all branch names combined' '
+    (cd repo && grit rev-list --all >../actual &&
+    grit rev-list master branchA branchB >../combined) &&
+    sort actual >sorted_all &&
+    sort combined >sorted_combined &&
+    test_cmp sorted_combined sorted_all
+'
+
+test_expect_success 'setup additional commit on master' '
+    (cd repo && git checkout master &&
+    echo extra >extra.txt && grit add extra.txt && grit commit -m "c4")
+'
+
+test_expect_success 'rev-list --all reflects new commit' '
+    (cd repo && grit rev-list --count --all >../actual) &&
+    echo 7 >expect &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --all --reverse --max-count=3' '
+    (cd repo && grit rev-list --all --reverse --max-count=3 >../actual) &&
+    (cd repo && git rev-list --all --reverse --max-count=3 >../expect) &&
+    test_cmp expect actual
+'
+
+test_expect_success 'rev-list --count --all matches line count' '
+    (cd repo && grit rev-list --all >../all_lines &&
+    grit rev-list --count --all >../count_val) &&
+    wc -l <all_lines >line_count &&
+    test_cmp count_val line_count
+'
+
+test_expect_success 'rev-list --all --skip exceeding total is empty' '
+    (cd repo && grit rev-list --all --skip=100 >../actual) &&
+    test_must_be_empty actual
+'
+
+test_done
