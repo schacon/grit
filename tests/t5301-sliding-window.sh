529c93e82193aa75bf5abaf3451c9f20c71e7275
New Name <test@example.com>
2026-04-02 04:36:46 +0000

    tests: deepen t5301-sliding-window.sh from 8 to 18 tests
    Add tests for multi-commit packs, verify-pack/show-index count agreement,
    known commit/tree/blob presence in verify-pack output, count-objects
    after repack, large pack handling (25+ files).

--- tests/t5301-sliding-window.sh
+++ tests/t5301-sliding-window.sh
@@ -67,4 +67,101 @@ test_expect_success 'verify-pack rejects nonexistent file' '
 	test_must_fail git verify-pack nonexistent.pack
 '
 
+# ---------------------------------------------------------------------------
+# Deepened: multi-commit pack
+# ---------------------------------------------------------------------------
+test_expect_success 'setup multi-commit pack' '
+	cd repo &&
+	echo two >two &&
+	"$REAL_GIT" update-index --add two &&
+	tree2=$("$REAL_GIT" write-tree) &&
+	commit2=$(echo commit2 | "$REAL_GIT" commit-tree "$tree2" -p HEAD) &&
+	git update-ref HEAD "$commit2" &&
+	echo three >three &&
+	"$REAL_GIT" update-index --add three &&
+	tree3=$("$REAL_GIT" write-tree) &&
+	commit3=$(echo commit3 | "$REAL_GIT" commit-tree "$tree3" -p HEAD) &&
+	git update-ref HEAD "$commit3" &&
+	"$REAL_GIT" repack -a -d
+'
+
+test_expect_success 'verify-pack -v shows all objects after multi-commit' '
+	cd repo &&
+	pack=$(echo .git/objects/pack/*.pack) &&
+	git verify-pack -v "$pack" >out &&
+	# At least: 3 commits + 3 trees + 3 blobs = 9
+	obj_count=$(grep -cE "^[0-9a-f]{40}" out) &&
+	test "$obj_count" -ge 9
+'
+
+test_expect_success 'verify-pack object count matches show-index count' '
+	cd repo &&
+	pack=$(echo .git/objects/pack/*.pack) &&
+	idx=${pack%.pack}.idx &&
+	verify_count=$(git verify-pack -v "$pack" | grep -cE "^[0-9a-f]{40}") &&
+	show_count=$(git show-index <"$idx" | wc -l) &&
+	test "$verify_count" = "$show_count"
+'
+
+test_expect_success 'verify-pack -v output contains known commit' '
+	cd repo &&
+	pack=$(echo .git/objects/pack/*.pack) &&
+	head_sha=$(git rev-parse HEAD) &&
+	git verify-pack -v "$pack" >out &&
+	grep "$head_sha" out
+'
+
+test_expect_success 'verify-pack -v output contains known tree' '
+	cd repo &&
+	pack=$(echo .git/objects/pack/*.pack) &&
+	tree_sha=$("$REAL_GIT" rev-parse HEAD^{tree}) &&
+	git verify-pack -v "$pack" >out &&
+	grep "$tree_sha" out
+'
+
+test_expect_success 'verify-pack -v output contains known blob' '
+	cd repo &&
+	pack=$(echo .git/objects/pack/*.pack) &&
+	blob_sha=$("$REAL_GIT" rev-parse HEAD:one) &&
+	git verify-pack -v "$pack" >out &&
+	grep "$blob_sha" out
+'
+
+test_expect_success 'count-objects shows 0 after second repack' '
+	cd repo &&
+	git count-objects >out &&
+	grep "^0 objects" out
+'
+
+# ---------------------------------------------------------------------------
+# Deepened: pack with many objects
+# ---------------------------------------------------------------------------
+test_expect_success 'setup large pack with many files' '
+	cd repo &&
+	for i in $(seq 1 25); do
+		echo "content $i" >"file$i.txt"
+	done &&
+	"$REAL_GIT" add . &&
+	tree4=$("$REAL_GIT" write-tree) &&
+	commit4=$(echo "many files" | "$REAL_GIT" commit-tree "$tree4" -p HEAD) &&
+	"$REAL_GIT" update-ref HEAD "$commit4" &&
+	"$REAL_GIT" repack -a -d
+'
+
+test_expect_success 'verify-pack handles large pack' '
+	cd repo &&
+	pack=$(echo .git/objects/pack/*.pack) &&
+	git verify-pack -v "$pack" >out &&
+	grep "^$pack: ok\$" out
+'
+
+test_expect_success 'show-index and verify-pack agree on large pack' '
+	cd repo &&
+	pack=$(echo .git/objects/pack/*.pack) &&
+	idx=${pack%.pack}.idx &&
+	show_count=$(git show-index <"$idx" | wc -l) &&
+	verify_count=$(git verify-pack -v "$pack" | grep -cE "^[0-9a-f]{40}") &&
+	test "$show_count" = "$verify_count"
+'
+
 test_done
