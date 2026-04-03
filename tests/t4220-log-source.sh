9261b524f06c13683080c68d62e63df46aad519d
Local User <schacon@gmail.com>
2026-04-03 19:18:40 +0000

    test(log): add tests for --no-walk, --date=<format>, --source, --simplify-by-decoration
    New test files:
    - t4218-log-no-walk.sh (7 tests)
    - t4219-log-date-format.sh (14 tests)
    - t4220-log-source.sh (4 tests)
    - t4221-log-simplify-decoration.sh (4 tests)
    
    All 29 new tests pass.

---
+++ tests/t4220-log-source.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+test_description='grit log --source
+
+Tests --source: show which ref led to each commit.'
+
+. ./test-lib.sh
+
+test_expect_success 'setup: repo with branches' '
+	git init repo &&
+	cd repo &&
+	git config user.name "A U Thor" &&
+	git config user.email "author@example.com" &&
+	test_tick &&
+	echo a >file.txt && git add file.txt && git commit -m "initial" &&
+	test_tick &&
+	echo b >file.txt && git add file.txt && git commit -m "second" &&
+	git checkout -b feature &&
+	test_tick &&
+	echo c >file.txt && git add file.txt && git commit -m "feature-commit" &&
+	git checkout master
+'
+
+test_expect_success '--source with --all shows ref names' '
+	cd repo &&
+	grit log --all --source --oneline >out &&
+	grep "feature" out
+'
+
+test_expect_success '--source output contains tab separator' '
+	cd repo &&
+	grit log --all --source --oneline >out &&
+	grep "	" out
+'
+
+test_expect_success '--source shows each commit with a ref' '
+	cd repo &&
+	grit log --all --source --oneline >out &&
+	# Every line should have a tab (source\thash subject)
+	while read line; do
+		echo "$line" | grep "	" || exit 1
+	done <out
+'
+
+test_done
