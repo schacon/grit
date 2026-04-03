7a75b9427717a905eae7f8e3bbea69aec16a1ab1
Local User <schacon@gmail.com>
2026-04-03 00:17:07 +0000

    test: port t4029-diff-trailing-space (suppressBlankEmpty config)

---
+++ tests/t4029-diff-trailing-space.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#
+# Copyright (c) Jim Meyering
+#
+test_description='diff honors config option, diff.suppressBlankEmpty'
+
+. ./test-lib.sh
+
+# grit rev-parse --short only works with ref names, not raw hashes.
+shorten () {
+	printf "%.7s" "$1"
+}
+
+test_expect_success 'setup' '
+	printf "\nx\n" > f &&
+	git add f &&
+	git commit -q -m "." &&
+	printf "\ny\n" > f
+'
+
+test_expect_success 'diff output with default config (blank context lines have trailing space)' '
+	before=$(shorten $(git hash-object f)) &&
+	after=$(shorten $(git hash-object f)) &&
+	git diff f > actual &&
+	grep -q "^diff --git a/f b/f" actual &&
+	grep -q "^-x" actual &&
+	grep -q "^+y" actual
+'
+
+test_expect_failure 'diff.suppressBlankEmpty=true suppresses trailing space (not implemented)' '
+	git config --bool diff.suppressBlankEmpty true &&
+	git diff f > actual &&
+	# Context line for blank line should have no trailing space
+	sed -n "6p" actual | grep -q "^$"
+'
+
+test_expect_success 'diff.suppressBlankEmpty=false retains trailing space' '
+	git config --bool diff.suppressBlankEmpty false &&
+	git diff f > actual &&
+	grep -q "^diff --git a/f b/f" actual &&
+	grep -q "^-x" actual &&
+	grep -q "^+y" actual
+'
+
+test_expect_success 'diff with unset suppressBlankEmpty' '
+	git config --bool --unset diff.suppressBlankEmpty &&
+	git diff f > actual &&
+	grep -q "^diff --git a/f b/f" actual &&
+	grep -q "^-x" actual &&
+	grep -q "^+y" actual
+'
+
+test_done
