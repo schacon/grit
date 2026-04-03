9c941b1d9b7cb182429db5d2215e87b39cfb78a2
Local User <schacon@gmail.com>
2026-04-03 00:33:54 +0000

    test: port t3000-ls-files-others from upstream git

--- tests/t3000-ls-files-others.sh
+++ tests/t3000-ls-files-others.sh
@@ -2,35 +2,15 @@
 #
 # Copyright (c) 2005 Junio C Hamano
 #
-# Ported from git/t/t3000-ls-files-others.sh
-# Tests that need --directory / --no-empty-directory are skipped (not implemented).
 
-test_description='basic tests for ls-files --others
-
-This test runs git ls-files --others with the following on the
-filesystem.
-
-    path0       - a file
-    path1	- a symlink
-    path2/file2 - a file in a directory
-    path3-junk  - a file to confuse things
-    path3/file3 - a file in a directory
-    path4       - an empty directory
-'
+test_description='basic tests for ls-files --others'
 
 . ./test-lib.sh
 
 test_expect_success 'setup' '
-	git init repo &&
-	cd repo &&
 	date >path0 &&
-	if test_have_prereq SYMLINKS
-	then
-		ln -s xyzzy path1
-	else
-		date >path1
-	fi &&
-	mkdir path2 path3 path4 &&
+	date >path1 &&
+	mkdir path2 path3 &&
 	date >path2/file2 &&
 	date >path2-junk &&
 	date >path3/file3 &&
@@ -38,59 +18,29 @@ test_expect_success 'setup' '
 	git update-index --add path3-junk path3/file3
 '
 
-test_expect_success 'ls-files --others' '
-	cd repo &&
-	git ls-files --others >output &&
+test_expect_success 'setup: expected output' '
 	cat >expected1 <<-\EOF &&
+	expected1
+	expected2
 	output
 	path0
 	path1
 	path2-junk
 	path2/file2
 	EOF
+
+	cp expected1 expected2
+'
+
+test_expect_success 'ls-files --others' '
+	git ls-files --others >output &&
 	test_cmp expected1 output
 '
 
-test_expect_failure 'ls-files --others --directory (not implemented)' '
-	cd repo &&
-	git ls-files --others --directory >output &&
-	cat >expected2 <<-\EOF &&
-	output
-	path0
-	path1
-	path2-junk
-	path2/
-	path4/
-	EOF
-	test_cmp expected2 output
-'
-
-test_expect_failure '--no-empty-directory hides empty directory (not implemented)' '
-	cd repo &&
-	git ls-files --others --directory --no-empty-directory >output &&
-	cat >expected3 <<-\EOF &&
-	output
-	path0
-	path1
-	path2-junk
-	path2/
-	EOF
-	test_cmp expected3 output
-'
-
 test_expect_success 'ls-files --others handles non-submodule .git' '
-	cd repo &&
-	rm -f expected1 expected2 expected3 &&
-	mkdir -p not-a-submodule &&
+	mkdir not-a-submodule &&
 	echo foo >not-a-submodule/.git &&
 	git ls-files -o >output &&
-	cat >expected1 <<-\EOF &&
-	output
-	path0
-	path1
-	path2-junk
-	path2/file2
-	EOF
 	test_cmp expected1 output
 '
 
