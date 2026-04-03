cfbf0a779be02b6f9cf72a9508214bad4dea9b66
Local User <schacon@gmail.com>
2026-04-03 19:07:00 +0000

    feat(diff): add --binary flag for GIT binary patch output
    - Add --binary flag to output 'GIT binary patch' blocks instead of
      'Binary files differ' messages
    - Implement write_git_binary_patch with deflate+base85 encoding
    - Add base85_encode helper function
    - Add test file t4078-diff-binary-flag.sh (5 tests)

---
+++ tests/t4078-diff-binary-flag.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+test_description='grit diff --binary
+
+Tests that --binary produces GIT binary patch output instead of
+"Binary files differ" message.'
+
+cd "$(dirname "$0")" || exit 1
+. ./test-lib.sh
+
+REAL_GIT=/usr/bin/git
+
+test_expect_success 'setup binary repo' '
+	$REAL_GIT init binrepo &&
+	cd binrepo &&
+	$REAL_GIT config user.name "Test" &&
+	$REAL_GIT config user.email "test@test.com" &&
+	printf "\x00\x01\x02\x03initial-binary" >bin.dat &&
+	echo "text content" >text.txt &&
+	$REAL_GIT add . &&
+	$REAL_GIT commit -m "initial"
+'
+
+test_expect_success 'diff --binary shows GIT binary patch' '
+	cd binrepo &&
+	printf "\x00\x01\x02\x04changed-binary" >bin.dat &&
+	$REAL_GIT add bin.dat &&
+	$REAL_GIT commit -m "modify binary" &&
+	grit diff --binary HEAD~1 HEAD >out &&
+	grep "GIT binary patch" out
+'
+
+test_expect_success 'diff --binary shows literal line' '
+	cd binrepo &&
+	grit diff --binary HEAD~1 HEAD >out &&
+	grep "^literal " out
+'
+
+test_expect_success 'diff without --binary shows Binary files differ' '
+	cd binrepo &&
+	grit diff HEAD~1 HEAD >out &&
+	grep "Binary files.*differ" out
+'
+
+test_expect_success 'diff --binary for added binary file' '
+	cd binrepo &&
+	printf "\x00\x01\x02" >new-bin.dat &&
+	$REAL_GIT add new-bin.dat &&
+	$REAL_GIT commit -m "add new binary" &&
+	grit diff --binary HEAD~1 HEAD >out &&
+	grep "GIT binary patch" out
+'
+
+test_done
