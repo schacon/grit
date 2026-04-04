ae3f09ed7d67f037274d0580f0d0915911cd1bee
Local User <schacon@gmail.com>
2026-04-04 04:28:38 +0000

    t5815: port submodule protocol restriction tests
    Rewrite 2 tests with real assertions for protocol.file.allow=never
    blocking clone and fetch operations. Both pass.

--- tests/t5815-submodule-protos.sh
+++ tests/t5815-submodule-protos.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # Ported from git/t/t5815-submodule-protos.sh
 # Tests for protocol restrictions with submodules
-#
-# Requires submodule + protocol.*.allow support. Stubbed.
 
 test_description='protocol restrictions with submodules'
 
@@ -12,7 +10,7 @@ export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
 cd "$(dirname "$0")" || exit 1
 . ./test-lib.sh
 
-test_expect_failure 'clone with submodule respects protocol.file.allow' '
+test_expect_success 'clone with submodule respects protocol.file.allow' '
 	test_create_repo sub &&
 	(cd sub && test_commit one) &&
 	test_create_repo super &&
@@ -22,11 +20,14 @@ test_expect_failure 'clone with submodule respects protocol.file.allow' '
 		git commit -m "add submodule"
 	) &&
 	test_must_fail git -c protocol.file.allow=never \
-		clone --recurse-submodules super clone-denied
+		clone --recurse-submodules super clone-denied 2>err &&
+	grep -i "not allowed" err
 '
 
-test_expect_failure 'fetch with submodule respects protocol.file.allow' '
-	false
+test_expect_success 'fetch with submodule respects protocol.file.allow' '
+	git clone super clone-for-fetch &&
+	test_must_fail git -C clone-for-fetch -c protocol.file.allow=never fetch 2>err &&
+	grep -i "not allowed" err
 '
 
 test_done
