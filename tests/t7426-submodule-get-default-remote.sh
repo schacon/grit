eb270bfcdee4145d1f0b87973adac2b682b5caeb
Local User <schacon@gmail.com>
2026-04-04 03:32:10 +0000

    test: port t7426-submodule-get-default-remote (stub — not yet implemented in grit)

---
+++ tests/t7426-submodule-get-default-remote.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#
+# Upstream: t7426-submodule-get-default-remote.sh
+# Requires submodule--helper which is not yet implemented in grit.
+# Stubbed as test_expect_failure.
+#
+
+test_description='git submodule--helper get-default-remote'
+
+GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main
+export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
+
+cd "$(dirname "$0")" || exit 1
+. ./test-lib.sh
+
+# --- submodule--helper not yet available in grit ---
+
+test_expect_failure 'setup repositories' '
+	false
+'
+
+test_expect_failure 'get-default-remote returns origin for initialized submodule' '
+	false
+'
+
+test_expect_failure 'get-default-remote works from subdirectory' '
+	false
+'
+
+test_expect_failure 'get-default-remote fails with non-existent path' '
+	false
+'
+
+test_expect_failure 'get-default-remote fails with non-submodule path' '
+	false
+'
+
+test_expect_failure 'get-default-remote fails without path argument' '
+	false
+'
+
+test_expect_failure 'get-default-remote fails with too many arguments' '
+	false
+'
+
+test_expect_failure 'get-default-remote returns non-origin remote name' '
+	false
+'
+
+test_expect_failure 'get-default-remote handles submodule with multiple remotes' '
+	false
+'
+
+test_expect_failure 'get-default-remote handles submodule with multiple remotes and none are origin' '
+	false
+'
+
+test_expect_failure 'get-default-remote works with nested submodule' '
+	false
+'
+
+test_expect_failure 'get-default-remote works with submodule that has no remotes' '
+	false
+'
+
+test_done
